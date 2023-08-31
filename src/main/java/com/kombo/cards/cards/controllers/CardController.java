87b45ff8d511efb554edf334de9897e397ccf1a0
Kombo <kombosteve9@gmail.com>
2023-08-31 20:27:42 +0300

    wip-creating cards endpoint

--- src/main/java/com/kombo/cards/cards/controllers/CardController.java
+++ src/main/java/com/kombo/cards/cards/controllers/CardController.java
@@ -1,15 +1,39 @@
 package com.kombo.cards.cards.controllers;
 
+import com.kombo.cards.cards.entities.CardDTO;
+import com.kombo.cards.cards.entities.CardRequest;
+import com.kombo.cards.cards.entities.CardResponse;
 import com.kombo.cards.cards.services.CardService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @Tag(name = "Card Manager")
 @AllArgsConstructor
 public class CardController {
-    private  final CardService cartService;
+    private  final CardService cardService;
+    @PostMapping("/cards/create")
+    @Operation(summary = "Create Card")
+    public ResponseEntity<CardResponse>create(@RequestBody CardRequest request){
+        CardDTO toCreate= new CardDTO();
+        toCreate.setColor(request.getColor());
+        toCreate.setDescription(request.getDescription());
+        toCreate.setName(request.getName());
+
+        CardDTO created=cardService.create(toCreate);
+
+        CardResponse response= new CardResponse();
+        response.setColor(created.getColor());
+        response.setName(created.getName());
+        response.setDescription(created.getDescription());
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/kombo/cards/cards/entities/Card.java
+++ src/main/java/com/kombo/cards/cards/entities/Card.java
@@ -1,7 +1,9 @@
 package com.kombo.cards.cards.entities;
 
+import com.kombo.cards.users.entities.User;
 import com.kombo.cards.utils.AuditModel;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Card extends AuditModel {
     @Column(nullable = false)
     private String name;
@@ -19,5 +22,5 @@ public class Card extends AuditModel {
     private  String description;
     private String publicId;
     private CardStatus status=CardStatus.TO_DO;
-
+   private User user;
 }
